a3e61d81f7154afa4cd2daa6fdbe8c1059ca9527
Muhammed Recep Hava <ragab5434@gmail.com>
2023-12-04 14:50:50 +0300

    created data base of book

--- book/models.py
+++ book/models.py
@@ -3,4 +3,7 @@ from django.db import models
 # Create your models here.
 
 
-
+class Book(models.Model):
+    title = models.CharField(max_length=100)
+    pages = models.CharField(max_length=3)
+    price = models.DecimalField(max_digits=5, decimal_places=2)
